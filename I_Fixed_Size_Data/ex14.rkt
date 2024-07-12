44d4ee1b996f0310898e3680a93c2ca921c002f4
programmint <programmint@proton.me>
2024-07-13 06:17:16 +0800

    提取字符串2

--- I_Fixed_Size_Data/ex14.rkt
+++ I_Fixed_Size_Data/ex14.rkt
@@ -1,9 +1,14 @@
-实现
-(define (string-last x)
-    (if(>=(string-length x) 1)
-        (substring x(- (string-length x) 1)(string-length x))
-        "请至少输入一个字母或数字"))
+; 14.1、题目
+; 定义 string-first 函数，它从非空字符串中提取最后一个 1 String。
 
- (string-last "abc")
-思路
-14题和13题的思路一致,不多述。
+; 14.2、实现
+(define (string-last str)
+  (substring str (- (string-length str ) 1 )))
+
+(string-last "abc")
+
+; 14.3、思路
+; 注意题目要求，是非空字符串。
+; 我最开始的解题方法，是用 cond 加以判断，输入的字符串是不是非空，如果是，则给出对应的提示，看似严谨，实则审题不严，题目中已经有了要求，要的是非空字符串。
+; 这个题目是很简单的题目。
+; 这里主要是用到了 substring
