3064767b7b1afa9a6a43a5c475ee92d2c81c32cf
programmint <programmint@proton.me>
2024-07-13 10:53:40 +0800

    小菜一碟

--- I_Fixed_Size_Data/ex2.rkt
+++ I_Fixed_Size_Data/ex2.rkt
@@ -1,11 +1,10 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 2.1、题目
+; 将 prefix 和 suffix 两个变量拼接起来（即拼串），并加上下划线"_"，最终的形成"hello_world"
 
-;2.1、实现
+; 2.2、实现
 (define prefix "hello")
 (define suffix "world")
 (string-append prefix "_" suffix)
 
-;2.2、思路解析
-;这个题目很简单,题目的思路,书上直接给出了,即运用：string-append,其实 append 这个函数 Python 也在运用;
+; 2.3、思路解析
+; 这个题目很简单，思书上直接给出了思路，即运用：string-append，其实 append 这个函数 Python 也在运用;
