f1cb40de41c0a8db17a353f92e6bb8ac86c7c29d
programmint <programmint@proton.me>
2024-07-13 08:44:12 +0800

    有点唬人

--- I_Fixed_Size_Data/ex31.rkt
+++ I_Fixed_Size_Data/ex31.rkt
@@ -1,3 +1,8 @@
+
+; 31.1、题目
+; 这题目太长，我就不引用了，请参阅
+
+; 31.2、实现
 (define (letter fst lst signature-name)
   (string-append
     (opening fst)
@@ -29,10 +34,15 @@
                       (read-file in-lst)
                       (read-file in-signature))))
 
-(write-file "first.dat" "Matt")
-(write-file "last.dat" "Johnson")
-(write-file "sig.dat" "Edward")
+(write-file "in-first.dat" "Matt")
+(write-file "in-last.dat" "Johnson")
+(write-file "in-sig.dat" "Edward")
 
-(main "first.dat" "last.dat" "sig.dat"  "letter.dat")
+(main "in-first.dat" "in-last.dat" "in-sig.dat"  "letter.dat")
 
 (read-file "letter.dat")
+
+; 31.3、思路解析
+; 这个题目其实有点唬人，核心就是考验刚刚所学的 write-file 和 read-file，注意 out 这个参数，不必另行给定一个实际的参数， out 是 write-file 用后面 letter 函数创建的。
+; 该书的题目，大多都有一共同点，就是需要识破题目的真正点，到底是什么。这一点，确实像产品设计，市场真正所需，不会明明白白告诉你，得需要你自己挖出来。
+
