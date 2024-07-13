058bc33b19f6e8d5fb3c3bded0207f1e9893f773
programmint <programmint@proton.me>
2024-07-13 11:44:04 +0800

    不要想复杂了

--- I_Fixed_Size_Data/ex6.rkt
+++ I_Fixed_Size_Data/ex6.rkt
@@ -1,18 +1,15 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex6) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
+; 6.1、题目
+; 添加图片，创建表达式，计算像素的数量。
 
-;6.1、实现
-
+; 6.2、实现
 (define cat (bitmap "images/cat.png")) 
 
 (* (image-heightt cat)(image-width cat))
 
-;6.2、思路解析
-;注意审题,编写像素的计算式。
-
-;我最开始是用函数来解决这个问题,很明显是考虑复杂了
-;不过这个题目,确实有一定的迷惑性,这里主要的目的,是写出对应的表达式
-;你当然可以写出一个函数,但题目的本意,就是助你掌握这个表达式。
-;毕竟,程序是为了解决需求。
+; 6.3、思路解析
+; 注意审题，编写像素的计算式。
+; 我最开始是用函数来解决这个问题，很明显是考虑复杂了
+; 不过这个题目，确实有一定的迷惑性，这里主要的目的，是写出对应的表达式
+; 你当然可以写出一个函数，但题目的本意，就是助你掌握这个表达式。
+; 毕竟，程序是为了解决需求。
