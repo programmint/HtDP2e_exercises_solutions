c3bd8bb15798418a0d0e0aa1cd3b324c03279f58
programmint <programmint@proton.me>
2024-07-13 10:36:57 +0800

    字符串与设计-1

--- I_Fixed_Size_Data/ex37.rkt
+++ I_Fixed_Size_Data/ex37.rkt
@@ -1,25 +1,28 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex37) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-; 1、String represents text . 信息转为数据
+; 37.1、题目
+; 设计函数 string-rest，生成一个与给定字符串相同的字符串，但去掉第一个字符。
 
-; 2、string -> string  函数签名
+; 37.2、实现
+    ; 37.2.1、String represents text . 信息转为数据
 
-; 3、Removes the last character but keeps the others 目的声明
+    ; 37.2.2、string -> string  函数签名
 
-; 4、(define (string-rest str)) 函数头
+    ; 37.2.3、Removes the last character but keeps the others 目的声明
 
-; 7、 函数示例
-; given: 
-;   str 是 "How to design programs" 
-; expected: 
-;   "ow to design programs" 
+    ; 37.2.4、(define (string-rest str)) 函数头
 
-; 7、(define (sting-rest str)  盘点（inventory）
-;            (... str ...))
+    ; 37.2.5、 函数示例
+    ; given: 
+    ;   str 是 "How to design programs" 
+    ; expected: 
+    ;   "ow to design programs" 
 
+    ; 37.2.6、(define (sting-rest str)  盘点（inventory）
+    ;            (... str ...))
 
-(define (sting-rest str)  
-           (substring str 1))
+    (define (sting-rest str)  
+            (substring str 1))
 
-(sting-rest "How to design programs") 
+    (sting-rest "How to design programs") 
+
+; 37.3、思路解析
+; 对应的思路，也是写在注释里面了。
