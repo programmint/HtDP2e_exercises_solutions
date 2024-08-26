bb7d808249366ca76b42743f614881a99797fb9a
programmint <programmint@proton.me>
2024-08-26 17:40:07 +0800

    依旧定义函数

---
+++ 1_Fixed_Size_Data/ex12.rkt
@@ -0,0 +1,16 @@
+; 12
+(define (cvolume x)
+    (expt x 3))
+
+(cvolume 3)
+
+; x 作为变量名，合适吗？
+; 不合适。x 到底表示什么意思，不明确
+; 搜索可知，边长的英文名是：edge length，所以变量名应是 edge_length，
+
+; 修改如下
+(define (cvolume edge_lenth)
+    (expt edge_length 3))
+
+(cvolume 3)
+
