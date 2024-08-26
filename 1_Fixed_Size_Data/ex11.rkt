fae13f777f7b3d8f2fcf567acfde95189f749539
programmint <programmint@proton.me>
2024-08-26 16:44:30 +0800

    定义函数

--- 1_Fixed_Size_Data/ex11.rkt
+++ 1_Fixed_Size_Data/ex11.rkt
@@ -8,7 +8,9 @@
 
 ; (define (FunctionName Variable ... Variable)
          Expression)
-; 要记住函数这个定义方法
+
+; 需要记住函数定义方法
+
 ; 从这一章，需要定义函数及变量。
 ; 怎么样命名，颇有技巧，所以 代码大全 2（纪念版） 第 11 章 《变量名称的威力》（The Power of Variable Names）专门讨论了这问题，可参考。
 ; Vscodium 或 vscode 有一插件 codelf ，可以从 github 上搜索对应的变量名，比对验证，以供参考。
