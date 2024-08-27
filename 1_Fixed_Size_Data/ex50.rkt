9beb898eb8f53a82204489c60844fa0ecbdb3b65
programmint <programmint@proton.me>
2024-08-27 15:13:07 +0800

    考测试案例是否完备

---
+++ 1_Fixed_Size_Data/ex50.rkt
@@ -0,0 +1,18 @@
+; 50
+
+; yields the next state given current state s
+; TrafficLight -> TrafficLight
+
+(check-expect (traffic-light-next "red") "green")
+(check-expect (traffic-light-next "green") "yellow")
+(check-expect (traffic-light-next "yellow") "red")
+
+(define (traffic-light-next s)
+  (cond
+    [(string=? "red" s) "green"]
+    [(string=? "green" s) "yellow"]
+    [(string=? "yellow" s) "red"]))
+
+; All 3 tests passed!
+
+
