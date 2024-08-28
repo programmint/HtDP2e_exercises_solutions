c8c152c29e26afcc957b3ac129544b200871cade
programmint <programmint@proton.me>
2024-08-28 14:10:14 +0800

    补增测试案例

--- 1_Fixed_Size_Data/ex50.rkt
+++ 1_Fixed_Size_Data/ex50.rkt
@@ -4,8 +4,8 @@
 ; TrafficLight -> TrafficLight
 
 (check-expect (traffic-light-next "red") "green")
-(check-expect (traffic-light-next "green") "yellow")  ; 新增测试案例
-(check-expect (traffic-light-next "yellow") "red")      ; 新增测试案例
+(check-expect (traffic-light-next "green") "yellow")  ; 补增测试案例
+(check-expect (traffic-light-next "yellow") "red")      ; 补增测试案例
 
 (define (traffic-light-next s)
   (cond
